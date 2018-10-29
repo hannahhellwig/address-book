5230d54f96d89346f8ff197a0c29b907a5960ef4
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-10-29 13:22:40 +0100

    get contacts array

--- code/server.js
+++ code/server.js
@@ -1,11 +1,32 @@
 import express from "express"
+import bodyParser from "body-parser"
+const contacts = require("./contacts.json")
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) =>
   res.send("Hello World!")
 )
 
-app.listen(3001, () =>
+app.get("/contacts", (req, res) => {
+  res.json(contacts)
+})
+
+app.get("/contacts/:id", (req, res) => {
+  const contactId = req.params.id
+  const filteredContacts = contacts.contacts.filter((item) => {
+    return (contactId == item.id)
+  })
+  res.json(filteredContacts)
+})
+
+app.get("*", (req, res) => {
+  res.send("404 page not found")
+})
+
+app.listen(3000, () =>
   console.log("Example app listening on port 3000!")
 )
